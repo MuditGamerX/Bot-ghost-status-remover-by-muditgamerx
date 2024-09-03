e33412c215f6f512d7c9af2c14c1bdb13511a737
MuditGamerX <muditgamerx@gmail.com>
2024-09-02 23:36:32 -0400

    index.js

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to Crown Smp: http://localhost:${port}`);
+  console.log(`🔗 Listening to Fallen Flare: http://localhost:${port}`);
   console.log(`🔗 Powered By Mr_MuditGamerX`);
 });
 
 
-const statusMessages = ["Watching Crown Smp","Playing Minecraft"];
+const statusMessages = ["Watching Fallen Flare","Playing Minecraft"];
 
 
 let currentIndex = 0;
